925feb55c756a77135af0966d23289bb5eeb6f42
yanchangbo <312210751@qq.com>
2026-04-29 21:12:50 +0800

    20260429

---
+++ src/scala/Test04_SingleLinkedList.scala
@@ -0,0 +1,17 @@
+package scala
+
+// 单向链表
+object Test04_SingleLinkedList {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+class SingleLinkedList() {
+
+}
+
+// 先创建HeroNode
+class HeroNode(no: Int,name: String,nikename: String) {
+
+}
