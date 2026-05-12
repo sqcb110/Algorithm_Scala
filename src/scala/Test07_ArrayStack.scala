376c4f815f6af7695e8b0df6e7052ea76406b30d
yanchangbo <312210751@qq.com>
2026-05-12 20:37:54 +0800

    20260512

--- src/scala/Test07_ArrayStack.scala
+++ src/scala/Test07_ArrayStack.scala
@@ -3,7 +3,13 @@ package scala
 // 用数组模拟栈
 object Test07_ArrayStack {
   def main(args: Array[String]): Unit = {
-
+    val stack = new ArrayStack(10)
+    for(i <- 0 to 9){
+      stack.push(i)
+    }
+    stack.pop()
+    stack.push(10)
+    stack.show()
   }
 }
 
@@ -14,12 +20,43 @@ class ArrayStack(size: Int) {
   var topIndex = -1
 
   // 判断栈是否满
+  def isFull(): Boolean = {
+    return topIndex == maxSize - 1
+  }
 
   // 判断栈是否空
+  def isEmpty(): Boolean = {
+    return topIndex == -1
+  }
 
   // 入栈
+  def push(n: Int): Unit = {
+    if(isFull()){
+      println("栈已满")
+    }else{
+      topIndex += 1
+      stack(topIndex) = n
+    }
+  }
 
   // 出栈
+  def pop(): Unit = {
+    if(isEmpty()){
+      println("栈已空")
+    }else{
+      stack(topIndex) = -1
+      topIndex -= 1
+    }
+  }
 
   // 遍历栈
+  def show(): Unit = {
+    if(isEmpty()){
+      println("栈为空")
+    }else{
+      for(i <- 0 to topIndex){
+        printf("下标%d的元素为%d\n",i,stack(i))
+      }
+    }
+  }
 }
