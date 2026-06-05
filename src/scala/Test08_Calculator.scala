d295c61b1ff10dd1a63b95007cdf0aab7c5ba51c
yanchangbo <312210751@qq.com>
2026-06-05 18:50:05 +0800

    20260605

--- src/scala/Test08_Calculator.scala
+++ src/scala/Test08_Calculator.scala
@@ -1,7 +1,6 @@
 package scala
-
 import scala.io.StdIn
-
+import scala.util.control.Breaks._
 /*
 用栈实现计算器的效果
 1、一个数字栈，一个运算符栈。
@@ -16,7 +15,7 @@ object Test08_Calculator {
   def main(args: Array[String]): Unit = {
     println("请输入表达式字符：")
     val str = StdIn.readLine()
-¬
+    printf("计算结果是：%d\n" , calculator(str))
   }
 
   // 计算方法
@@ -27,8 +26,13 @@ object Test08_Calculator {
 
     // 把表达式拆成字符
     val charArray = disassembleString(expression)
+
     // 把字符放入两个栈
-    putCharIntoStack(charArray,numStack,optStack)
+    putCharIntoStacks(charArray, numStack, optStack)
+
+    // 出栈计算
+    popCalculate(numStack, optStack)
+
   }
 
   // 把表达式拆成字符
@@ -37,14 +41,70 @@ object Test08_Calculator {
   }
 
   // 把字符放入两个栈
-  def putCharIntoStack(arr: Array[Char],stack1: StackNumOrOperator,stack2: StackNumOrOperator): Unit = {
+  def putCharIntoStacks(arr: Array[Char], stack1: StackNumOrOperator, stack2: StackNumOrOperator): Unit = {
+    val numArray = Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+    val optArray = Array('+', '-', '*', '/')
 
+    for (i <- 0 until arr.length) {
+      if (numArray.contains(arr(i).toString)) {
+        // 如果是数字，直接入数栈。
+        stack1.push(arr(i).toString.toInt)
+      } else if (optArray.contains(arr(i))) {
+        // 如果是符号，则：
+        if (!stack2.isEmpty && getOptPriority(stack2.stack(stack2.topIndex).toChar) <= getOptPriority(arr(i))) {
+          // 4.1 如果符号栈有数据，且当前符号优先级小于等于符号栈栈顶符号，则符号栈栈顶出栈，数字栈出栈两个数进行运算，运算结果进数字栈。
+          val optTop = stack2.pop().toChar
+          val num1 = stack1.pop()
+          val num2 = stack1.pop()
+          val resultMid = optTop match {
+            case '*' => num2 * num1
+            case '/' => num2 / num1
+            case '+' => num2 + num1
+            case '-' => num2 - num1
+            case _   => throw new IllegalArgumentException(s"不支持的运算符: $optTop")
+          }
+          stack1.push(resultMid)
+          stack2.push(arr(i).toInt)
+        } else if (stack2.isEmpty || getOptPriority(stack2.stack(stack2.topIndex).toChar) > getOptPriority(arr(i))) {
+          // 4.2 如果当前符号栈没有数据，直接入栈
+          // 4.3 反之，则符号栈入栈
+          stack2.push(arr(i).toInt)
+        }
+      }
+    }
+  }
+
+  // 出栈计算最终结果
+  def popCalculate(nStack: StackNumOrOperator, oStack: StackNumOrOperator): Int = {
+      while (!oStack.isEmpty() && nStack.topIndex >= 1) {
+        val num1 = nStack.pop()
+        val num2 = nStack.pop()
+        val opt = oStack.pop().toChar
+        val midResult = opt match {
+          case '*' => num2 * num1
+          case '/' => num2 / num1
+          case '+' => num2 + num1
+          case '-' => num2 - num1
+          case _   => throw new IllegalArgumentException(s"不支持的运算符: $opt")
+        }
+          nStack.push(midResult)
+      }
+    nStack.pop()
+  }
+
+  // 计算符号优先级
+  def getOptPriority(opt: Char): Int = {
+    if (opt == '*' | opt == '/') {
+      return 0
+    } else{
+      return 1
+    }
   }
 }
 
 class StackNumOrOperator(size: Int) {
   val maxSize = size
-  var stack = new Array[Int](maxSize)
+  var stack = new Array[Int](maxSize) // 如果用arraybuffer，无容量限制
   // 栈顶的索引，默认为-1，栈空的时候同样为-1
   var topIndex = -1
 
@@ -60,31 +120,34 @@ class StackNumOrOperator(size: Int) {
 
   // 入栈
   def push(n: Int): Unit = {
-    if(isFull()){
+    if (isFull()) {
       println("栈已满")
-    }else{
+    } else {
       topIndex += 1
       stack(topIndex) = n
     }
   }
 
   // 出栈
-  def pop(): Unit = {
-    if(isEmpty()){
+  def pop(): Int = {
+    if (isEmpty()) {
       println("栈已空")
-    }else{
+      -1
+    } else {
+      val tmp = stack(topIndex)
       stack(topIndex) = -1
       topIndex -= 1
+      tmp
     }
   }
 
   // 遍历栈
   def show(): Unit = {
-    if(isEmpty()){
+    if (isEmpty()) {
       println("栈为空")
-    }else{
-      for(i <- 0 to topIndex){
-        printf("下标%d的元素为%d\n",i,stack(i))
+    } else {
+      for (i <- 0 to topIndex) {
+        printf("下标%d的元素为%d\n", i, stack(i))
       }
     }
   }
